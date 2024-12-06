846c094bf17afeac596d1c924db3de2b973e4de1
Kamil Dusejovsky <74691395+dusek2@users.noreply.github.com>
2024-12-06 05:20:46 -0800

    Update contact.tsx

--- site/components/sections/contact.tsx
+++ site/components/sections/contact.tsx
@@ -11,7 +11,7 @@ import emailjs from 'emailjs-com';
 
 export function Contact() {
   const [formData, setFormData] = useState({
-    name: '',
+    name: ,
     email: '',
     message: '',
   });
@@ -30,10 +30,10 @@ export function Contact() {
 
     try {
       const result = await emailjs.send(
-        '', // Replace with your EmailJS Service ID
-        '', // Replace with your EmailJS Template ID
+        import.meta.env.VITE_EMAILJS_SERVICE_ID as string, // Replace with your EmailJS Service ID
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Replace with your EmailJS Template ID
         formData,
-        '' // Replace with your EmailJS User ID
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY // Replace with your EmailJS User ID
       );
       console.log(result.text);
       setFeedback('Message sent successfully!');
